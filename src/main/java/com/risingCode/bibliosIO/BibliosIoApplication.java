716eff43c5324710eb36052b71c4249359046e56
Amonnuns <ramonwx15@gmail.com>
2022-08-16 20:30:53 -0300

    Bcrypt encoder added as a bean.

--- src/main/java/com/risingCode/bibliosIO/BibliosIoApplication.java
+++ src/main/java/com/risingCode/bibliosIO/BibliosIoApplication.java
@@ -2,12 +2,19 @@ package com.risingCode.bibliosIO;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class BibliosIoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BibliosIoApplication.class, args);
+	public static void main(String[] args) {SpringApplication.run(BibliosIoApplication.class, args);}
+
+	@Bean
+	public PasswordEncoder getPasswordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 
 }
