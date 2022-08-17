781e15d6f413c0749c6ee45da866a5bc92a471ad
Amonnuns <ramonwx15@gmail.com>
2022-08-17 16:18:51 -0300

    Service notation added

--- src/main/java/com/risingCode/bibliosIO/services/UserService.java
+++ src/main/java/com/risingCode/bibliosIO/services/UserService.java
@@ -4,7 +4,9 @@ import com.risingCode.bibliosIO.exceptions.UserAlreadyCreatedException;
 import com.risingCode.bibliosIO.models.User;
 import com.risingCode.bibliosIO.repository.UserRepository;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserService {
 
     private final UserRepository userRepository;
