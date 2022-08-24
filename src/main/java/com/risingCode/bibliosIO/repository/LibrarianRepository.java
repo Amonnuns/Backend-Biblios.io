dde5d7bdf57bc7e66a52ad7ad1ac485d3dc04097
Amonnuns <ramonwx15@gmail.com>
2022-08-24 16:44:48 -0300

    LibrarianRepository created

---
+++ src/main/java/com/risingCode/bibliosIO/repository/LibrarianRepository.java
@@ -0,0 +1,14 @@
+package com.risingCode.bibliosIO.repository;
+
+import com.risingCode.bibliosIO.models.Librarian;
+import com.risingCode.bibliosIO.models.Reader;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface LibrarianRepository extends JpaRepository<Librarian, UUID> {
+
+    Optional<Librarian> findByUsernameAndPassword(String username, String password);
+    Optional<Librarian> findByUsername(String username);
+}
