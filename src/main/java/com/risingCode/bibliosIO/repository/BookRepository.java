98fc2976e9d50103cd884c7ead440859ae8ebae1
Amonnuns <ramonwx15@gmail.com>
2022-08-25 11:43:13 -0300

    DTOs to handle Book validation added

---
+++ src/main/java/com/risingCode/bibliosIO/repository/BookRepository.java
@@ -0,0 +1,10 @@
+package com.risingCode.bibliosIO.repository;
+
+import com.risingCode.bibliosIO.models.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface BookRepository extends JpaRepository<Book, UUID> {
+}
