81ded7d48b84160e1266f03d41c9122bf98b9df3
Amonnuns <ramonwx15@gmail.com>
2022-08-29 10:49:39 -0300

    AuthenticationUser Controller test

--- src/test/java/com/risingCode/bibliosIO/controllers/ReaderControllerTest.java
+++ src/test/java/com/risingCode/bibliosIO/controllers/ReaderControllerTest.java
@@ -2,16 +2,24 @@ package com.risingCode.bibliosIO.controllers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.risingCode.bibliosIO.dto.UserDTO;
+import com.risingCode.bibliosIO.dto.UserLoginFormDto;
 import com.risingCode.bibliosIO.repository.ReaderRepository;
 import com.risingCode.bibliosIO.services.ReaderService;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -26,9 +34,6 @@ class ReaderControllerTest {
 
     @MockBean
     private ReaderService readerService;
-
-    @Mock
-    private ReaderRepository readerRepository;
 
 
     @Test
@@ -52,6 +57,20 @@ class ReaderControllerTest {
 
     @Test
     void authenticateUser() throws Exception {
-        //todo
+        String username = "test";
+        String password = "testepass";
+
+        UserLoginFormDto user = new UserLoginFormDto(username, password);
+
+        String body = objectMapper.writeValueAsString(user);
+
+        when(readerService.authenticateReader(any(UserLoginFormDto.class)))
+                .thenReturn(true);
+
+
+        mvc.perform(post("/api/v1/user/auth")
+                .contentType("application/json")
+                .content(body))
+                .andExpect(status().isAccepted());
     }
 }
