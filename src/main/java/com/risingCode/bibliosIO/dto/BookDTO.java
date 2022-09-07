54d5ec4a10a2e852d09f91b92d936b3a63fe1e39
Amonnuns <ramonwx15@gmail.com>
2022-09-07 18:31:54 -0300

    1 - Constructor created 2 - Title size increased to 100 3 - Year corrected to digits validation constraint 4 - NumberOfPages corrected to NotNull and Now uses max and Min constraints max now is no digits but maximum value that is 3000 5 - getters and setters created

--- src/main/java/com/risingCode/bibliosIO/dto/BookDTO.java
+++ src/main/java/com/risingCode/bibliosIO/dto/BookDTO.java
@@ -3,26 +3,63 @@ package com.risingCode.bibliosIO.dto;
 import com.risingCode.bibliosIO.models.Author;
 
 import javax.persistence.Column;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Size;
+import javax.validation.constraints.*;
 import java.util.HashSet;
 import java.util.Set;
 
 public class BookDTO {
 
+    public BookDTO(String title, int year, int numberOfPages, Set<AuthorDTO> authors) {
+        this.title = title;
+        this.year = year;
+        this.numberOfPages = numberOfPages;
+        this.authors = authors;
+    }
+
     @NotBlank
-    @Size(max = 50,
+    @Size(max = 100,
             message = "Book title can be more than 50 characters long")
     private String title;
 
-    @Size(max = 4,
-            message = "Year can be more than 4 characters long")
+    @Digits(integer = 4, message ="year can't be higher than 4 digits", fraction = 0)
     private int year;
 
-    @NotBlank
-    @Size(max = 4,
-            message = "Number of pages can be more than 4 characters long")
+    @NotNull
+    @Max(value = 3000, message ="Number of pages can't be higher than 3000" )
+    @Min(value = 0l, message = "Can't be a negative number")
     private int numberOfPages;
 
     private Set<AuthorDTO> authors = new HashSet<AuthorDTO>();
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getNumberOfPages() {
+        return numberOfPages;
+    }
+
+    public void setNumberOfPages(int numberOfPages) {
+        this.numberOfPages = numberOfPages;
+    }
+
+    public Set<AuthorDTO> getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(Set<AuthorDTO> authors) {
+        this.authors = authors;
+    }
 }
